a44f2362780bbb8d1807a88c045d0d2aa1a54675
Vishrok <43007093+Vishrok@users.noreply.github.com>
2019-09-27 09:43:15 -0600

    Added Database

---
+++ DDL/CreateDatabase.sql
@@ -0,0 +1,11 @@
+-- A single line comment is one that starts with 2 dashes
+--CREATE DATABASE MyDatabase
+--GO -- Used to indicate that the above code should run as a "batch"
+--DROP DATABASE MyDatabase
+--GO
+
+--[ctrl] + r - toggles messages/results window
+
+USE MyDatabase -- Change the execution content for the script that follows
+GO -- run as batch
+
